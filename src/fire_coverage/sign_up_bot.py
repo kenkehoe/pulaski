e3192c1188bb9ffaced11faabf3eabaacce079de
Alex Olivas <alex.r.olivas@gmail.com>
2018-07-11 16:17:21 -0600

    refactored using latest incarnations of GMail and GCalendar classes.

--- src/fire_coverage/sign_up_bot.py
+++ src/fire_coverage/sign_up_bot.py
@@ -1,10 +1,9 @@
-import logging
 
 from enum import Enum
 from os.path import join
 from os import environ
 
-def get_volunteer_names(event_summary):
+def get_volunteer_names_from_summary(event_summary):
     '''
     Expects a summary of the form "Night (<volunteer1>, <volunteer2>, ...)"
     This returns a list of strings, split on ',', for everything between the parentheses.
@@ -13,156 +12,83 @@ def get_volunteer_names(event_summary):
     end = event_summary.find(')')
     volunteer_names = event_summary[start+1:end].strip()
     return [name for name in volunteer_names.split(',') if name]
-
-def is_today(date):
+        
+def extract_email(string):
     '''
-    Date from gmail is of the form 'Thu, 5 Jul 2018 20:04:33 -0600'
+    Expects a string like 'Rando Volunteer <email>'
     '''
-    import datetime
-    today = datetime.date.today()
-    
-    labels = ['jan','feb','mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
-    _month = {k:v+1 for v,k in enumerate(labels)} 
-    
-    ds = date.split()
-    year = int(ds[3])
-    month = _month[ds[2].lower()]
-    day = int(ds[1])
-
-    return bool(datetime.date(year, month, day) == today)
-    
-    
-def parse_volunteer_list(volunteers):
-    '''
-    Expects a list of 'Rando Volunteer <email>'
-    '''
-    result = list()
-    for v in volunteers:
-        if '<' in v and '>' in v:
-            begin = v.rfind('<')
-            end = v.find('>')
-            email = v[begin+1: end]
-        else:
-            email = v
-        assert('@' in email)
-        result.append(email)
-    return result                      
-
-gcal_color_map = {'yellow': 5,
-                  'orange': 6,
-                  'green': 10,
-                  'red': 11}
+    email = None
+    if '<' in string and '>' in string:
+        begin = string.rfind('<')
+        end = string.find('>')
+        email = string[begin+1: end]
+    else:
+        email = string
+    assert('@' in email)
+    return email
 
 class SignUpBot:
 
     def __init__(self,
                  gcal_name = 'Nederland Fire',
                  config_path = join(environ['HOME'], '.fire_coverage')):
-                 
-        from apiclient.discovery import build
-        from httplib2 import Http
-        from oauth2client import file, client, tools
 
-        self.gcal_name = gcal_name
+        import logging
+        from .members import Members
+        from .gsuite.gmail import GMail
+        from .gsuite.gcalendar import GCalendar
+
         self.logger = logging.getLogger(__name__)
+
+        credentials_path = join(config_path, 'nfv_gmail_credentials.json')
+        secret_credentials_path = join(config_path, 'nfv_gmail_client_secret.json')
+        scopes = GMail.Scopes.READONLY
+        self.gmail = GMail(credentials_path, secret_credentials_path, scopes)
+
+        credentials_path = join(config_path, 'gcal_credentials.json')
+        secret_credentials_path = join(config_path, 'gcal_client_secret.json')
+        scopes = GCalendar.Scopes.MODIFY
+        self.gcal = GCalendar(credentials_path, secret_credentials_path, scopes)        
+        self.gcal.select_calendar(gcal_name)
+                                     
+        members_filename = join(config_path, 'members')
+        self.members = Members(members_filename)
+                        
+    def check_email(self, test = False):
+
+        import datetime
         
-        # Setup the GMail API
-        SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
-        store = file.Storage(join(config_path, 'nfv_gmail_credentials.json'))
-        creds = store.get()
-        if not creds or creds.invalid:
-            flow = client.flow_from_clientsecrets(join(config_path,'nfv_gmail_client_secret.json'), SCOPES)
-            creds = tools.run_flow(flow, store)
-        self.gmail_service = build('gmail', 'v1', http=creds.authorize(Http()))
-                             
-
-        # Setup the Calendar API
-        SCOPES = 'https://www.googleapis.com/auth/calendar'
-        store = file.Storage(join(config_path, 'gcal_credentials.json'))
-        creds = store.get()
-        if not creds or creds.invalid:
-            flow = client.flow_from_clientsecrets(join(config_path,'gcal_client_secret.json'), SCOPES)
-            creds = tools.run_flow(flow, store)
-        self.gcal_service = build('calendar', 'v3', http=creds.authorize(Http()))
-
-
-        self.__set_gcal_id()
-        assert(self.gcal_id)        
-                             
-        member_list_path = join(config_path, 'member_list')
-        self.member_email_dict = dict()
-        with open(member_list_path) as f:
-            for line in f.readlines():
-                if not line :
-                    continue
-                name = line.split(':')[0]
-                email = line.split(':')[1].strip()
-                nickname = line.split(':')[2].strip()
-                self.member_email_dict[email] = nickname
-                
-        self.volunteers = set()
-
-    def __set_gcal_id(self):
-        '''
-        Sets self.gcal_id to the ID matching self.gcal_name
-        '''
-        page_token = None
-        while True:
-            calendar_list = self.gcal_service.calendarList().list(pageToken=page_token).execute()
-            for calendar_list_entry in calendar_list['items']:
-                if calendar_list_entry['summary'] == self.gcal_name:          
-                    self.gcal_id = calendar_list_entry['id']
-            page_token = calendar_list.get('nextPageToken')
-            if not page_token:
-                break
-
-        
-    def check_email(self):
-
         self.logger.debug("checking email...")
         
-        from apiclient import errors
-        import datetime
-        today = datetime.date.today()
+        messages = self.gmail.messages_list(q = 'is:unread')
 
-        try:
-            # Apparently in google's world yesterday is still *after* today...go figure.
-            # This does still limit the number of responses however.  I'll file a bug report 
-            # at some point.  I'm sure this is not intended behavior
-            query = 'subject:NedFire Volunteers AND after:%s' % str(today)
-            response  = self.gmail_service.users().messages().list(userId='me', q = query).execute()
-        except errors.HttpError as error:
-            print('An error occured: %s' % error)
-            
-        if response['resultSizeEstimate'] == 0 :
+        emails = list()
+        if messages and messages['resultSizeEstimate'] == 0 :
             self.logger.info('No responses found.')
         else:
             
-            start_size = len(self.volunteers)
-            volunteers = list()
+            t = datetime.date.today()
+            start_of_today = datetime.datetime(t.year, t.month, t.day)
+            
+            for message in messages['messages']:
 
-            for message in response['messages']:
-                m = self.gmail_service.users().messages().get(userId='me', id = message['id']).execute()
-                print('\n',m['payload'], '\n')
-                vname = None
-                sent_today = False
-                for d in m['payload']['headers']:                
-                    if d['name'] == 'From':
-                        vname = d['value']
-                    if d['name'] == 'Date':
-                        self.logger.debug('*** date = %s' % d['value'])
-                        self.logger.debug('    is_today = %s' % is_today(d['value']))
-                        sent_today = is_today(d['value'])
-                if sent_today and vname:
-                    volunteers.append(vname)
-                    
-            volunteers.extend(self.volunteers)
-            self.volunteers = set(volunteers)
+                # Only need the header data for now.
+                m = self.gmail.messages_get(id = message['id'], format = 'metadata')
+                if not test:
+                    # Mark this email as 'read' but only when live
+                    self.gmail.messages_modify(id = m['id'], body = {'removeLabelIds': ['UNREAD']})
+                
+                date_received = datetime.datetime.fromtimestamp(int(m['internalDate'])/1e3)
+                if date_received > start_of_today:                
+                    for d in m['payload']['headers']:                
+                        if d['name'] == 'From':
+                            email = extract_email(d['value'])
+                            if email != 'nedfire.vols@gmail.com':
+                                emails.append(email)
+                        
+        return emails
 
-            if len(self.volunteers) > start_size:
-                self.update_calendar()
-
-    def update_calendar(self):
+    def get_night_shift_event(self):
 
         import datetime
         
@@ -170,41 +96,37 @@ class SignUpBot:
         one_microsecond = datetime.timedelta(microseconds=1)
         # one microsecond before the stroke of midnight
         end_of_today = datetime.datetime(now.year, now.month, now.day + 1) - one_microsecond
-        
-        events_result = self.gcal_service.events().list(calendarId = self.gcal_id,
-                                                        timeMin = now.isoformat() + 'Z',
-                                                        timeMax = end_of_today.isoformat() + 'Z',
-                                                        maxResults = 20,
-                                                        singleEvents=True,
-                                                        orderBy = 'startTime').execute()
 
-        # these should be sorted in order
-        events = events_result.get('items', [])
+        events = self.gcal.events(timeMin = now.isoformat() + 'Z',
+                                  timeMax = end_of_today.isoformat() + 'Z',
+                                  maxResults = 20,
+                                  singleEvents=True,
+                                  orderBy = 'startTime')
         
-        coverage = False
-        night_shift = None
         for event in events:
             if 'Night' in event['summary']:
-                night_shift = event
-                break
+                return event
 
-        volunteers = get_volunteer_names(night_shift['summary'])
-        start_size = len(volunteers)
-        print(parse_volunteer_list(self.volunteers))
-        volunteers.extend([self.member_email_dict[e] for e in parse_volunteer_list(self.volunteers)])
-        if len(set(volunteers)) > start_size:
-            # we need an update
-            # we have new volunteers                
-            volunteers = [v+',' for v in set(volunteers)]
-            vlist = ''.join(volunteers).rstrip(',')
+    def update_calendar(self, email_list, test = False):
+
+        night_shift = self.get_night_shift_event()        
+        names = get_volunteer_names_from_summary(night_shift['summary'])
+        start_size = len(names)
+
+        names.extend([self.members.members[email] for email in email_list])
+        if len(set(names)) > start_size:
+            # we have new volunteers, so we need an update
+            volunteers = [v+',' for v in set(names)]
+            vlist = ''.join(volunteers).rstrip(',') # remove the last comma
             summary = 'Night (%s)' % vlist
             self.logger.debug('summary = %s' % summary)
             
-            night_shift['colorId'] = gcal_color_map['green']
+            night_shift['colorId'] = GCalendar.ColorID.BASIL.value
             night_shift['summary'] = summary
-            self.gcal_service.events().update(calendarId = self.gcal_id,
-                                              eventId = night_shift['id'],
-                                              body = night_shift).execute()
+            if not test:
+                self.gcal.update_event(night_shift)
+            return True
+        return False
                         
     def go(self, frequency = 10):
         '''
@@ -215,5 +137,7 @@ class SignUpBot:
         self.logger.debug("go")
         
         while True:
-            self.check_email()
+            email_list = self.check_email()
+            if email_list:
+                self.update_calendar(email_list)
             time.sleep(frequency * 60)
