b9ac398412f7cc5ca20dd0d839b501beca5f25bd
Alex Olivas <alex.r.olivas@gmail.com>
2018-07-11 17:03:26 -0600

    adapted alerts class to use new gsuite classes.

--- src/fire_coverage/alerts.py
+++ src/fire_coverage/alerts.py
@@ -1,103 +1,103 @@
 
-def get_volunteer_names(event_summary):
-    '''
-    Expects a summary of the form "Night (<volunteer1>, <volunteer2>, ...)"
-    This returns a list of strings, split on ',', for everything between the parentheses.
-    '''
-    start = event_summary.find('(')
-    end = event_summary.find(')')
-    volunteer_names = event_summary[start+1:end].strip()
-    return [name for name in volunteer_names.split(',') if name]
-
-gcal_color_map = {'yellow': 5,
-                  'orange': 6,
-                  'green': 10,
-                  'red': 11}
-
-
+from enum import Enum
 from os.path import join
 from os import environ
 
 class NightCoverageAlerts:
 
+    class AlertLevel(Enum):
+        NO_ALERT = 0
+        CAUTION = 1
+        WARNING = 2
+        SEVERE = 3
+    
     def __init__(self,
                  gcal_name = 'Nederland Fire',
-                 config_path = join(environ['HOME'], '.fire_coverage')):
+                 config_path = join(environ['HOME'], '.fire_coverage'),
+                 test = False):
 
-        from apiclient.discovery import build
-        from httplib2 import Http
-        from oauth2client import file, client, tools
         import logging
         
-        self.gcal_name = gcal_name
-        self.logger = logging.getLogger(__name__)
-        
-        # Setup the Calendar API
-        SCOPES = 'https://www.googleapis.com/auth/calendar'
-        store = file.Storage(join(config_path, 'gcal_credentials.json'))
-        creds = store.get()
-        if not creds or creds.invalid:
-            flow = client.flow_from_clientsecrets(join(config_path,'gcal_client_secret.json'), SCOPES)
-            creds = tools.run_flow(flow, store)
-        self.service = build('calendar', 'v3', http=creds.authorize(Http()))
+        from .members import Members
+        from .gsuite.gmail import GMail
+        from .gsuite.gcalendar import GCalendar
 
-        self.__set_gcal_id()
-        assert(self.gcal_id)        
+        self.logger = logging.getLogger(__name__)
+
+        credentials_path = join(config_path, 'nfv_gmail_credentials.json')
+        secret_credentials_path = join(config_path, 'nfv_gmail_client_secret.json')
+        scopes = GMail.Scopes.READONLY
+        self.gmail = GMail(credentials_path, secret_credentials_path, scopes)
+
+        credentials_path = join(config_path, 'gcal_credentials.json')
+        secret_credentials_path = join(config_path, 'gcal_client_secret.json')
+        scopes = GCalendar.Scopes.MODIFY
+        self.gcal = GCalendar(credentials_path, secret_credentials_path, scopes)        
+        self.gcal.select_calendar(gcal_name)
+                                     
+        members_filename = join(config_path, 'members')
+        self.members = Members(members_filename)
+        self.test = test
                 
     def __send_alert(self, alert_level):
-        from fire_coverage.gmail import GMail        
-        email_client = GMail()        
-        email_client.send_alert(alert_level, 'Test')
         
-    def __set_gcal_id(self):
-        '''
-        Sets self.gcal_id to the ID matching self.gcal_name
-        '''
-        page_token = None
-        while True:
-            calendar_list = self.service.calendarList().list(pageToken=page_token).execute()
-            for calendar_list_entry in calendar_list['items']:
-                if calendar_list_entry['summary'] == self.gcal_name:          
-                    self.gcal_id = calendar_list_entry['id']
-            page_token = calendar_list.get('nextPageToken')
-            if not page_token:
-                break
+        if alert_level == self.AlertLevel.NO_ALERT:
+            to = 'alex.r.olivas@gmail.com'
+            body = "All is good." 
+            subject = '[NedFire Volunteers] NFPD is covered tonight' 
 
-    def go(self, test = False):
+        if alert_level == self.AlertLevel.CAUTION:
+            to = 'alex.r.olivas@gmail.com'
+            body = 'I hope someone signs up today.'
+            subject = '[NedFire Volunteers] No coverage tonight yet.'
+
+        if alert_level == self.AlertLevel.WARNING:
+            to = ','.join(self.members.members.keys())
+            body = "To sign up for tonight's shift, simply respond to this email."
+            subject = '[NedFire Volunteers] Please help out tonight. '
+                
+        if alert_level == self.AlertLevel.SEVERE:
+            to = ','.join(self.members.members.keys())
+            body = "To sign up for tonight's shift, simply respond to this email."
+            subject = '[NedFire Volunteers] PLEEEAAASE help out tonight. Pretty please?'
+
+        if not self.test:
+            self.gmail.send_message(to, subject, body)
+            
+    def get_night_shift_event(self):
 
         import datetime
-        from fire_coverage.gmail import AlertLevel        
         
         now = datetime.datetime.utcnow()
         one_microsecond = datetime.timedelta(microseconds=1)
         # one microsecond before the stroke of midnight
         end_of_today = datetime.datetime(now.year, now.month, now.day + 1) - one_microsecond
-        
-        events_result = self.service.events().list(calendarId = self.gcal_id,
-                                                   timeMin = now.isoformat() + 'Z',
-                                                   timeMax = end_of_today.isoformat() + 'Z',
-                                                   maxResults = 20,
-                                                   singleEvents=True,
-                                                   orderBy = 'startTime').execute()
 
-        # these should be sorted in order
-        events = events_result.get('items', [])
+        events = self.gcal.events(timeMin = now.isoformat() + 'Z',
+                                  timeMax = end_of_today.isoformat() + 'Z',
+                                  maxResults = 20,
+                                  singleEvents=True,
+                                  orderBy = 'startTime')
         
-        coverage = False
-        night_shift = None
         for event in events:
             if 'Night' in event['summary']:
-                night_shift = event
-                break
-                
+                return event
 
-        volunteers = get_volunteer_names(night_shift['summary'])
+        
+    def go(self):
+
+        from .utilities import get_volunteer_names_from_summary
+        from .gsuite.gcalendar import GCalendar
+        
+        night_shift = self.get_night_shift_event()
+        
+        volunteers = get_volunteer_names_from_summary(night_shift['summary'])
         if volunteers:
             self.logger.debug("The following are signed up for tonight:")
-            for vol in volunteers:
-                self.logger.debug("   %s" % vol)
-                self.__send_alert(AlertLevel.NO_ALERT)
-                event_color = 'green'                    
+            for v in volunteers:
+                self.logger.debug("   %s" % v)
+                self.__send_alert(self.AlertLevel.NO_ALERT)
+                event_color_id = GCalendar.ColorID.BASIL
         else:
             today = datetime.datetime.today()
             noon = datetime.datetime(today.year, today.month, today.day, 11, 59)
@@ -106,17 +106,16 @@ class NightCoverageAlerts:
             
             self.logger.debug('No coverage.')
             if now < noon:
-                event_color = 'yellow'
-                self.__send_alert(AlertLevel.CAUTION)
+                event_color_id = GCalendar.ColorID.BANANA
+                self.__send_alert(self.AlertLevel.CAUTION)
             if now > noon and now < eightpm:
-                event_color = 'orange'
-                self.__send_alert(AlertLevel.WARNING)
+                event_color_id = GCalendar.ColorID.TANGERINE
+                self.__send_alert(self.AlertLevel.WARNING)
             if now > eightpm:
-                event_color = 'red'
-                self.__send_alert(AlertLevel.SEVERE)
+                event_color_id = GCalendar.ColorID.TOMATO
+                self.__send_alert(self.AlertLevel.SEVERE)
                 
-        night_shift['colorId'] = gcal_color_map[event_color]
-        self.service.events().update(calendarId = self.gcal_id,
-                                     eventId = night_shift['id'],
-                                     body = night_shift).execute()
+        night_shift['colorId'] = event_color_id.value
+        if not self.test:
+            self.gcal.update_event(night_shift)
  
